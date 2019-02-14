117ce114767002f2c6e46efaf31f44f738af41aa
Top Deuse <topdeuse@gmail.com>
2019-02-14 10:54:49 +0300

    Updates

---
+++ precache-manifest.02773c43e8c270142a3d339c719da21a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dd7aad877980f1a4620",
+    "url": "/test-calc/static/js/runtime~main.1dd7aad8.js"
+  },
+  {
+    "revision": "6aa9a19b53013eb54ecc",
+    "url": "/test-calc/static/js/main.6aa9a19b.chunk.js"
+  },
+  {
+    "revision": "1db9f7111577a09d2afd",
+    "url": "/test-calc/static/js/2.1db9f711.chunk.js"
+  },
+  {
+    "revision": "6aa9a19b53013eb54ecc",
+    "url": "/test-calc/static/css/main.865d0e74.chunk.css"
+  },
+  {
+    "revision": "044abbcc6ed53fb542a0fcb071b53502",
+    "url": "/test-calc/index.html"
+  }
+];
